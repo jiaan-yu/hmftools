38c8d6a080e8272460803d7af6eeab73b1a13a6e
Nevhan Ramadan <me@binaryminary.tech>
2017-09-18 15:55:37 +1000

    use jaxb

--- bachelor/src/test/java/BachelorTest.java
+++ bachelor/src/test/java/BachelorTest.java
@@ -1,19 +1,44 @@
 import static org.junit.Assert.assertTrue;
 
+import java.io.File;
 import java.io.IOException;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+
 import com.google.common.io.Resources;
-import com.hartwig.hmftools.bachelor.BachelorSchema;
+import com.hartwig.hmftools.bachelor.Programs;
 
 import org.junit.Test;
 import org.xml.sax.SAXException;
 
 public class BachelorTest {
 
-    private final static String TEST_XML = Resources.getResource("test.xml").getPath();
+    private final static String TEST_XML = Resources.getResource("valid.xml").getPath();
+    private final static String TEST_INVALID_XML = Resources.getResource("invalid.xml").getPath();
 
     @Test
-    public void TestXML() throws SAXException, IOException {
-        assertTrue(BachelorSchema.validateXML(TEST_XML));
+    public void TestValid() throws JAXBException, IOException, SAXException {
+
+        final JAXBContext context = JAXBContext.newInstance(com.hartwig.hmftools.bachelor.Programs.class);
+        final Unmarshaller unmarshaller = context.createUnmarshaller();
+        final Programs programs = (Programs) unmarshaller.unmarshal(new File(TEST_XML));
+
+        assertTrue(programs != null);
+    }
+
+    @Test
+    public void TestInvalid() throws JAXBException, IOException, SAXException {
+        final JAXBContext context = JAXBContext.newInstance(com.hartwig.hmftools.bachelor.Programs.class);
+        final Unmarshaller unmarshaller = context.createUnmarshaller();
+
+        Programs programs = null;
+        try {
+            programs = (Programs) unmarshaller.unmarshal(new File(TEST_INVALID_XML));
+        } catch (final JAXBException e) {
+        }
+
+        assertTrue(programs == null);
     }
 }
