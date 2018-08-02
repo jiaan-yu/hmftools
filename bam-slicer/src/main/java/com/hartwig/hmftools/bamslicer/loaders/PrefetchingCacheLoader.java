4b8b547638dd3c5317f98f904fb4b6b12e50afe7
Mircea Voda <mircearaul.voda@gmail.com>
2018-08-02 16:04:15 +0200

    trim logging for prefetching cacheLoader

--- bam-slicer/src/main/java/com/hartwig/hmftools/bamslicer/loaders/PrefetchingCacheLoader.java
+++ bam-slicer/src/main/java/com/hartwig/hmftools/bamslicer/loaders/PrefetchingCacheLoader.java
@@ -5,13 +5,9 @@ import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
 
 import org.apache.commons.lang3.tuple.Pair;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
 
 public class PrefetchingCacheLoader extends CacheLoader<Pair<Long, Long>, ListenableFuture<byte[]>> {
-    private static final Logger LOGGER = LogManager.getLogger(PrefetchingCacheLoader.class);
-
     @NotNull
     private final ChunkByteLoader loader;
 
@@ -22,7 +18,6 @@ public class PrefetchingCacheLoader extends CacheLoader<Pair<Long, Long>, Listen
     @Override
     @NotNull
     public ListenableFuture<byte[]> load(@NotNull final Pair<Long, Long> range) {
-        LOGGER.info("Cache-loading bytes at: {} - {}", range.getLeft(), range.getRight());
         return loader.getBytes(range.getLeft(), range.getRight());
     }
 
@@ -30,7 +25,6 @@ public class PrefetchingCacheLoader extends CacheLoader<Pair<Long, Long>, Listen
     @NotNull
     public ListenableFuture<ListenableFuture<byte[]>> reload(@NotNull final Pair<Long, Long> range,
             @NotNull final ListenableFuture<byte[]> oldBytes) {
-        LOGGER.info("Cache re-loading bytes at: {} - {}", range.getLeft(), range.getRight());
         return Futures.immediateFuture(oldBytes);
     }
 }
